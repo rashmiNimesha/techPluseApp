e9a349cfbf6c931181ff043aae46bec79de948d1
rashmiNimesha <90458280+rashmiNimesha@users.noreply.github.com>
2025-06-01 09:20:17 +0530

    techPluse: implementation of devinfor and EditUser screen

--- app/src/main/java/com/example/techpluseapplication/DevInfoActivity.java
+++ app/src/main/java/com/example/techpluseapplication/DevInfoActivity.java
@@ -1,24 +1,27 @@
 package com.example.techpluseapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class DevInfoActivity extends AppCompatActivity {
+
+    ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dev_info);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(v -> {
+            Intent i = new Intent(DevInfoActivity.this, DashboardActivity.class);
+            startActivity(i);
+            finish();
         });
     }
 }
--- app/src/main/java/com/example/techpluseapplication/UserInfoActivity.java
+++ app/src/main/java/com/example/techpluseapplication/UserInfoActivity.java
@@ -1,24 +1,38 @@
 package com.example.techpluseapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class UserInfoActivity extends AppCompatActivity {
+
+    ImageButton back;
+    Button editUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_info);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        back = findViewById(R.id.backButton);
+        editUser = findViewById(R.id.editUserButton);
+
+        back.setOnClickListener(v -> {
+            Intent i = new Intent(this, DashboardActivity.class);
+            startActivity(i);
+            finish();
         });
+
+        editUser.setOnClickListener(v -> {
+            Intent i = new Intent(this, EditUserActivity.class);
+            startActivity(i);
+            finish();
+        });
+
     }
 }
